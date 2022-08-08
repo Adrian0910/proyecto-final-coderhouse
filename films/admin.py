38c888af9436dca10e46a6ff4242de63824ea5ea
Adrian0910 <adriansantander60@aragon.unam.mx>
2022-08-07 23:09:15 -0500

    Se creo user admin para visualizar los datos en path admin

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from films.models import Film
 # Register your models here.
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'year', 'director', 'actors', 'description']
+
--- films/models.py
+++ films/models.py
@@ -4,12 +4,13 @@ from django.db import models
 class Film(models.Model):
     name = models.CharField(max_length=40)
     price = models.FloatField()
-    year = models.FloatField()
+    year = models.IntegerField()
     director = models.CharField(max_length=40)
     actors = models.CharField(max_length=80)
     description = models.CharField(max_length=300)
     
-
+    def __str__(self):
+        return self.name
 
 
 
