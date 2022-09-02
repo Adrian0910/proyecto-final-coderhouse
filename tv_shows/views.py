5332cf6c175c7db3f0cb6c6de327612754be8434
Adrian0910 <adriansantander60@aragon.unam.mx>
2022-09-02 00:15:22 -0500

    Funcion delete ya es funcional

--- tv_shows/views.py
+++ tv_shows/views.py
@@ -5,28 +5,26 @@ from tv_shows.forms import Form_tv_shows
 from tv_shows.models import Tv_shows
 
 
-
-
 def create_tv_show(request):
-    
+
     if request.method == 'POST':
         form = Form_tv_shows(request.POST)
 
         if form.is_valid():
             Tv_shows.objects.create(
-                name = form.cleaned_data['name'],
-                price = form.cleaned_data['price'],
-                year = form.cleaned_data['year'],
-                director = form.cleaned_data['director'],
-                actors = form.cleaned_data['actors'],
-                description = form.cleaned_data['description'],
-                
-            )  
-            return redirect (list_tv_shows)
+                name=form.cleaned_data['name'],
+                price=form.cleaned_data['price'],
+                year=form.cleaned_data['year'],
+                director=form.cleaned_data['director'],
+                actors=form.cleaned_data['actors'],
+                description=form.cleaned_data['description'],
+
+            )
+            return redirect(list_tv_shows)
 
     elif request.method == 'GET':
         form = Form_tv_shows()
-        context = {'form':form}
+        context = {'form': form}
         return render(request, 'tv_shows.html', context=context)
 
 
@@ -41,9 +39,9 @@ def list_tv_shows(request):
 def delete_tv_show(request, pk):
     if request.method == 'GET':
         tv_shows = Tv_shows.objects.get(pk=pk)
-        context = {'tv_shows':tv_shows}
+        context = {'tv_shows': tv_shows}
         return render(request, 'delete_tv_show.html', context=context)
     elif request.method == 'POST':
         tv_shows = Tv_shows.objects.get(pk=pk)
         tv_shows.delete()
-        return redirect(list_tv_shows.html)
+        return redirect(list_tv_shows)
