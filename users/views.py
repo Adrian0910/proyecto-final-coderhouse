e6b779b5bc8e9ba21f3129e1ecb3a2f9fd477ebf
Adrian0910 <adriansantander60@aragon.unam.mx>
2022-09-05 00:51:00 -0500

    Registro de usuarios finalziado

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
 
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
+
+from users.forms import User_registration_form
 
 
 # Create your views here.
@@ -32,9 +35,17 @@ def login_request(request):
 
 def register(request):
     if request.method == 'POST':
-        pass
+        form = User_registration_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        context = {'errors':form.errors}
+        form = User_registration_form()
+        context['form'] = form
+        return render(request, 'users/register.html', {'form': form})
+
     elif request.method == 'GET':
-        form = UserCreationForm()
+        form = User_registration_form()
         return render(request, 'users/register.html', {'form': form})
 
 
